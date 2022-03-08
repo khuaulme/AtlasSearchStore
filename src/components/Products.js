d1357bc6855020f0430ba700270c7194ad44c1ec
Karen Huaulme <karen.huaulme@gmail.com>
2022-03-08 15:38:28 +0000

    added product component for all products - no search yet

---
+++ src/components/Products.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Product from "./Product";
+
+const Products = ({ products }) => {
+  return (
+    <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
+      {products.map((product, idx) => (
+        // <div key={idx}>working</div>
+        <Product key={product._id} product={product} />
+      ))}
+    </div>
+  );
+};
+
+export default Products;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Container from "../components/Container";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Pagination from "../components/Pagination";
+import Products from "../components/Products";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
 
-  //  useEffect(async () => {
+  const getProducts = async () => {
+    let productsReturned = await (
+      await fetch(
+        `https://us-east-1.aws.data.mongodb-api.com/app/searchstore-zhtzd/endpoint/products`
+      )
+    ).json();
+    setProducts(productsReturned.products);
+    console.log(productsReturned.products.length);
+  };
+
+  useEffect(() => {
+    getProducts();
+    console.log("GETTING PRODUCTS");
+  }, []); // add all external values your effect function depends on - none in this case
+
+  // useEffect(async () => {
   //    // add your Realm App Id to the .env.local file
   //    const REALM_APP_ID = process.env.NEXT_PUBLIC_REALM_APP_ID;
   //    const app = new Realm.App({ id: REALM_APP_ID });
   //    const credentials = Realm.Credentials.anonymous();
-  //    try {
-  //      const user = await app.logIn(credentials);
-  //      const allProducts = await user.functions.getAllProducts();
-  //      setProducts(() => allProducts);
-  //      const uniqueCategories = await user.functions.getUniqueCategories();
-  //      setCategories(() => uniqueCategories);
-  //    } catch (error) {
-  //      console.error(error);
-  //    }
-  //  }, []);
+  //   try {
+  // const user = await app.logIn(credentials);
+  // const allProducts = await user.functions.getAllProducts();
+  // setProducts(() => allProducts);
+  // const uniqueCategories = await user.functions.getUniqueCategories();
+  // setCategories(() => uniqueCategories);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }, []);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -36,8 +52,8 @@ const Home = () => {
             category="Tech Wear"
             categories={categories}
             productCount={`${products.length} Products`}
-          />
-          <Products products={products} />*/}
+          /> */}
+          <Products products={products} />
           <Pagination />
         </Container>
         <Footer />
