6eec8c15d4e295aee73b7fbeab22b5f590abb243
Karen Huaulme <karen.huaulme@gmail.com>
2022-03-08 18:57:50 +0000

    radio for categories

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Pagination from "../components/Pagination";
 import Products from "../components/Products";
+import CategoryRadio from "../components/CategoryRadio";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
@@ -26,28 +27,13 @@ const Home = () => {
     console.log("GETTING PRODUCTS");
   }, []); // add all external values your effect function depends on - none in this case
 
-  // useEffect(async () => {
-  //    // add your Realm App Id to the .env.local file
-  //    const REALM_APP_ID = process.env.NEXT_PUBLIC_REALM_APP_ID;
-  //    const app = new Realm.App({ id: REALM_APP_ID });
-  //    const credentials = Realm.Credentials.anonymous();
-  //   try {
-  // const user = await app.logIn(credentials);
-  // const allProducts = await user.functions.getAllProducts();
-  // setProducts(() => allProducts);
-  // const uniqueCategories = await user.functions.getUniqueCategories();
-  // setCategories(() => uniqueCategories);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }, []);
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <div className="bg-white w-full min-h-screen">
         <Header />
         <Container>
           <Hero />
+          <CategoryRadio />
           {/* <Category
             category="Tech Wear"
             categories={categories}
