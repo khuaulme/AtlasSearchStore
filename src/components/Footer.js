ab4cc54285827bae21d91264a4646404364ecca1
Karen Huaulme <karen.huaulme@gmail.com>
2022-09-16 22:54:56 -0500

    last search commit before branching for kafka demo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-green-600">
       <div className="container mx-auto px-6 py-3 flex justify-between items-center">
         <div className="text-xl font-bold  text-white cursor-pointer">
-          Use Atlas Search
+          Current 2022: The Next Generation of Kafka Summit
         </div>
         <p className="py-2 text-gray-200 sm:py-0">All rights reserved</p>
       </div>
